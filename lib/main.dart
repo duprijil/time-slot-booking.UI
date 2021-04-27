5e941a2f4a69854fe075221c6c27daa61b5d9652
Dumitru <d.prijilevschi@gmail.com>
2021-04-27 14:38:14 +0300

    change theme to light

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class TimeSlotBooking extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Time Slot Booking',
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
       home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
